e188472a7b182e204c8951a876ebec1f0de99a68
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-29 16:51:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     req = Rack::Request.new(env)
    
     if req.path.match(/item/)
-      item_price = req.path.split('item')
+      item_price = req.path.split('/item/')
    
    
   end 
