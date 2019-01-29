180fa99fb7e6974e035698eed0ba395bea186588
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-29 17:03:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
         resp.write "Item not found"
       end 
     else 
-      resp.write "Route not Found"
+      resp.write "Route not found"
       resp.status = 404
       
     end 
